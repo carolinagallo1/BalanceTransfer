83b8f579c702a653553fbce5e19059af29846446
carolinagallo <c.gallo1190@pascualbravo.edu.co>
2025-03-31 01:07:53 -0500

    Added dbcontext for conection with sql

---
+++ BalanceTransfer/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BalanceTransfer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BalanceTransfer.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+
+        public DbSet<Wallet> Wallet { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transactions>()
+                .HasOne<Wallet>()
+                .WithMany()
+                .HasForeignKey(t => t.WalletId)
+                .IsRequired();
+
+        }
+
+    }
+}
--- BalanceTransfer/Program.cs
+++ BalanceTransfer/Program.cs
@@ -1,6 +1,18 @@
+using BalanceTransfer.Interfaces;
+using BalanceTransfer.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
+
+builder.Services.AddScoped<IWalletRepository, WalletRepository>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,6 +26,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
